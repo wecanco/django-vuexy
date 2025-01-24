50d894622649cca8d6100bb78f1197f84d4b7ef4
WeCan <mohammad.boveyri@basalam.com>
2025-01-24 18:31:35 +0330

    init

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from django.utils.translation import gettext_lazy as _
 from dotenv import load_dotenv
 
-from .template import TEMPLATE_CONFIG, THEME_LAYOUT_DIR, THEME_VARIABLES
+from template import TEMPLATE_CONFIG, THEME_LAYOUT_DIR, THEME_VARIABLES
 
 load_dotenv()  # take environment variables from .env.
 
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
-    
+
     "web_project.language_middleware.DefaultLanguageMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
